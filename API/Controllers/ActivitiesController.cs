92506778df17047d75684c2cebf124445dd91c02
Tomisalav Robić <tomislav.robic.eu@gmail.com>
2025-01-28 15:44:00 +0100

    End of Section 10

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -10,35 +10,32 @@ namespace API.Controllers
         [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()//(CancellationToken ct)
         {
-            return await Mediator.Send(new List.Query()); //, cancellationToken: ct);
+            return HandleResult(await Mediator.Send(new List.Query())); //, cancellationToken: ct);
         }
 
         [HttpGet("{id}")] //api/activities/{id}
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await Mediator.Send(new Details.Query { Id = id });
+            return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
         }
 
         [HttpPost] //api/activities
-        public async Task<ActionResult> CreateActivity(Activity activity)
+        public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            await Mediator.Send(new Create.Command { Activity = activity });
-            return Ok();
+            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
         [HttpPut("{id}")] //api/activities/{id}
-        public async Task<ActionResult> EditActivity(Guid id, Activity activity)
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
-            await Mediator.Send(new Edit.Command { Activity = activity });
-            return Ok();
+            return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
 
         [HttpDelete("{id}")] //api/activities/{id}
-        public async Task<ActionResult> DeleteActivity(Guid id)
+        public async Task<IActionResult> DeleteActivity(Guid id)
         {
-            await Mediator.Send(new Delete.Command { Id = id });
-            return Ok();
+            return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -13,6 +14,8 @@ var app = builder.Build();
 app.UseCors("CorsPolicy");
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
