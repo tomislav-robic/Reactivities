0a5ac4dbe9c970bfcd385f4bcc0f063f5f254bcf
Tomisalav Robić <tomislav.robic.eu@gmail.com>
2025-02-04 15:57:07 +0100

    End of Section 11

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -59,14 +59,9 @@ const requests = {
 }
 
 const Activities = {
-    list: (): Promise<Activity[]> => requests.get<Activity[]>('/activities').then(activities =>{
-        return activities.map(activity => {
-            activity.date = activity.date.split('T')[0];
-            return activity;
-        });
-    }),
+    list: (): Promise<Activity[]> => requests.get<Activity[]>('/activities'),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => requests.post<void>('/activities', activity),
+    create: (activity: Activity) => requests.post<Activity>('/activities', activity),
     update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
     delete: (id: string) => requests.del<void>(`/activities/${id}`)
 }
